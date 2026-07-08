abc49e95db531ce9e37ca0140605d8984ee43f8f
Wilson Beima <wilbeimer@gmail.com>
2026-07-08 19:34:36 -0400

    test: add course creation endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from backend.main import app
+from unittest.mock import patch
 
 client = TestClient(app)
 
@@ -11,3 +12,31 @@ def test_root():
     assert response.json() == {
         "message": "Hello World"
     }
+
+
+def test_get_nonexistent_course():
+    response = client.get("/courses/does-not-exist")
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Course not found"
+    }
+
+
+@patch("backend.main.generate_curriculum")
+def test_post_course(mock_generate):
+    data = {
+        "name": "test_course_name",
+        "color": "test_course_color"
+    }
+
+    response = client.post("/courses", json=data)
+    assert response.status_code == 200
+    body = response.json()
+
+    assert id in body
+    assert body["name"] == "test_course_name"
+    assert body["color"] == "test_course_color"
+    assert body["status"] == "pending"
+
+    mock_generate.assert_called_once()
